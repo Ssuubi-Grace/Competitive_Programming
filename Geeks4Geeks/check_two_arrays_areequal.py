7ef3c34abb9068d16f142f54b6d12b59a4ba614c
Ssuubi-Grace <112149184+Ssuubi-Grace@users.noreply.github.com>
2024-02-13 20:52:17 +0530

    Create check_two_arrays_areequal.py

---
+++ Geeks4Geeks/check_two_arrays_areequal.py
@@ -0,0 +1,11 @@
+class Solution:
+    #Function to check if two arrays are equal or not.
+    def check(self,A,B,N):
+        A.sort()
+        B.sort()
+
+        for i in range(N):
+            if A[i] != B[i]:
+                return False
+
+        return True
