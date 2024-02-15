cd55d99b5160928efed6cc6663c164571341ade4
Ssuubi-Grace <112149184+Ssuubi-Grace@users.noreply.github.com>
2024-02-15 17:43:05 +0530

    Create Merge_String_Alternatively.py

---
+++ Leetcode/Merge_String_Alternatively.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        i, j = 0, 0
+        merged = ""
+
+        while i < len(word1) and j < len(word2):
+            merged += word1[i] + word2[j]
+            i += 1
+            j += 1
+
+        merged += word1[i:] + word2[j:]
+
+        return merged   
